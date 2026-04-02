95be875105728adede64512cf76de4a5251e42a1
jcmullwh <78822128+jcmullwh@users.noreply.github.com>
2026-04-02 16:57:00 -0400

    chore: clean up recovery artifacts (#156)

--- .pyfix/sitecustomize.py
+++ .pyfix/sitecustomize.py
@@ -1,8 +1,8 @@
-﻿import os
+import os
 
 # Work around sandboxed filesystems where os.mkdir(path, mode=0o700) yields a non-writable directory.
 # On standard Windows/NTFS, the mode argument is effectively ignored.
-if os.name == 'nt':
+if os.name == "nt":
     _orig_mkdir = os.mkdir
 
     def _patched_mkdir(path, mode=0o777, *, dir_fd=None):
